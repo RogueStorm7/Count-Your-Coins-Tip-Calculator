48b72413417b40136c754fbe8aa62ac9db79e0f3
unknown <RogueStorm7>
2022-04-01 18:13:45 -0700

    changes to tip caluculator

--- Count-Your-Coins-Tip-Calculator.py
+++ Count-Your-Coins-Tip-Calculator.py
@@ -2,26 +2,26 @@ print('Welcome to Count Your Coins Tip Calculator!!!')
 
 #asks user what the tab total is
 total_tab = float(input('What is the total bill?\n'))
-
+sales_tax = .06 + total_tab
+tip_percentage = .20
+#prompt of what the tip will be?
+tip = total_tab * tip_percentage
+print(sales_tax)
+print()
+print(tip)
 print ("How was the service? Please use good or bad.")
 service = input()
 
-#prompt of what the tip will be?
-if service == "good":
-    tip_percentage = total_tab * 0.20
-else:
-    tip_percentage = total_tab * 0.15
-tip = total_tab / 1.08 * tip_percentage
-tip = int(input('How much tip would you like to give? 15, or feeling generous 20?\n'))
 
+tip = float(input('How much tip would you like to give?'))
 #how many people will you be splitting the bill with?
-num = int(input('How many people to split the bill?\n'))
+num = float(input('How many people to split the bill?\n'))
 
 #tip percentage divided by 100
 tip_percentage = tip / 100
 
-total_tip = total_tab * tip_percentage
-total_tab = total_tab + total_tip
+# total_tip = total_tab * tip_percentage
+total_tab = total_tab + tip
 tab_per_person = total_tab/ num
 
 grand_total = "{:.2f}".format(tab_per_person)
